74bb0446c7b62047a2459711b5b9146bcd3e8db1
Abel Carvajal <abel.carvajalgil@bt.com>
2019-03-07 18:14:02 +0100

    ficheros subido por mi

---
+++ Jenk_test2.groovy
@@ -0,0 +1,31 @@
+pipeline {
+    agent any
+    stages {
+        stage('Fase 1') {
+            when {
+                branch 'master'
+            }
+            steps {
+                echo 'Fase 1'
+                sh 'touch file_Fase1.txt'
+                sh 'echo "Esto es la fase 1" >> file_Fase1.txt'
+
+            }   
+        }
+        stage('Fase 2'){
+            steps {
+                echo 'Fase 2'
+                sh 'touch file_Fase2.txt'
+                sh 'echo "Esto es la Fase2" >> file_Fase2.txt'
+            }
+        }
+        stage('Fase Final'){
+            steps {
+                echo 'Fase Final'
+                sh 'touch file_Final.txt'
+                sh 'cat file_Fase1.txt >> file_Final.txt'
+                sh 'cat file_Fase2.txt >> file_Final.txt'
+            }
+        }
+    }
+}
